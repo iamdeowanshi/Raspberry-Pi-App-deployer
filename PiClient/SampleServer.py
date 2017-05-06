1f97f37d90d66f71ca996b45beddc7db2240b7dd
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-05-06 12:00:24 -0700

    Refactoring code

--- PiClient/SampleServer.py
+++ PiClient/SampleServer.py
@@ -1,12 +1,11 @@
 import pika
 import uuid
+
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
 channel.queue_declare(queue='pi')
-
 result = channel.queue_declare(queue='pi_ip')
-
 callback_queue = result.method.queue
 
 def get_app_url():
@@ -14,10 +13,8 @@ def get_app_url():
 
 def callback(ch,method, props, body):
     print(" [x] Received %r" % body)
-    #print("Request Received...")
     response = get_app_url()
     print response
-
     corr_id = str(uuid.uuid4())
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
@@ -32,11 +29,9 @@ def callback(ch,method, props, body):
 def callResult(ch,method,props,body):
     print body
 	
-channel.basic_consume(callback,
-                      queue='pi',
-                      no_ack=True)
+channel.basic_consume(callback, queue='pi', no_ack=True)
 
-channel.basic_consume(callResult, queue='pi_ip',no_ack=True)
+channel.basic_consume(callResult, queue='pi_ip', no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
