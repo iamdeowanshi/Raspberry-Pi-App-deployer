dca986842fac46f0cc5a69470651b4bb72105158
Aaditya Deowanshi <aadityadeowanshi@hotmail.com>
2017-05-07 20:17:24 -0700

    Added ip list api and Ui ip suggestion drop down

--- ClientSide/UI/assets/js/scripts.js
+++ ClientSide/UI/assets/js/scripts.js
@@ -1,73 +1,89 @@
+window.ip_list = [];
+jQuery(document).ready(function () {
+    // jQuery.ajax({
+    //     type: "GET",
+    //     url: "http://104.196.235.71/deployer/v1/list",
+    //     dataType: "json",
+    //     success: function (data, textStatus, jqXHR) {
+    //         data.forEach(function (element) {
+    //             window.ip_list.push(element);
+    //         }, this);
+    //         console.log(data.length + textStatus);
+    //     }, //function(data, textStatus, jqXHR)
+    //     error: function (jqXhr, textStatus, errorThrown) {
+    //         console.log(errorThrown); //function(jqXHR, textStatus, errorThrown)
+    //     }
+    // });
 
-jQuery(document).ready(function() {
-	
     /*
         Fullscreen background
     */
     $.backstretch("assets/img/backgrounds/1.jpg");
-    
+
     /*
         Form validation
     */
-/*    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
-    	$(this).removeClass('input-error');
-    });*/
-    
-     $('#login-form').submit(function(e) {
+    /*    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+        	$(this).removeClass('input-error');
+        });*/
+
+    $('#login-form').submit(function (e) {
         e.preventDefault();
         var git_repo = document.getElementById("login-form")[1].value;
-        var  ip = document.getElementById("login-form")[0].value;
+        var ip = document.getElementById("login-form")[0].value;
         console.log(git_repo);
         jQuery.ajax({
-         type: "POST",
-         url: "http://104.196.235.71/deployer/v1/" + ip + "/deploy",
-         data: '{ "git_url": "' + git_repo + '"}',
-         dataType:"json",
-         contentType: "application/json",
-         success: function(data, textStatus, jqXHR) {
-            console.log(data.message);
-            showOutput(data.message, textStatus);
-         }, //function(data, textStatus, jqXHR)
-         error: function(jqXhr, textStatus, errorThrown) {
-         console.log(errorThrown); //function(jqXHR, textStatus, errorThrown)
-         }
+            type: "POST",
+            url: "http://104.196.235.71/deployer/v1/" + ip + "/deploy",
+            data: '{ "git_url": "' + git_repo + '"}',
+            dataType: "json",
+            contentType: "application/json",
+            success: function (data, textStatus, jqXHR) {
+                console.log(data.message);
+                showOutput(data.message, textStatus);
+            }, //function(data, textStatus, jqXHR)
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown); //function(jqXHR, textStatus, errorThrown)
+            }
         });
     });
 
-function myFunction() {
-    var  ip = document.getElementById("login-form")[0].value;
-    jQuery.ajax({
-        type: "GET",
-        url: "http://104.196.235.71/deployer/v1/" + ip + "/status",
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data + textStatus);
-            document.getElementById("demo").innerHTML = data.message;
-        }, //function(data, textStatus, jqXHR)
-        error: function (jqXhr, textStatus, errorThrown) {
-            console.log(errorThrown); //function(jqXHR, textStatus, errorThrown)
-        }
+    function myFunction() {
+        var ip = document.getElementById("login-form")[0].value;
+        jQuery.ajax({
+            type: "GET",
+            url: "http://104.196.235.71/deployer/v1/" + ip + "/status",
+            dataType: "json",
+            success: function (data, textStatus, jqXHR) {
+                console.log(data + textStatus);
+                document.getElementById("demo").innerHTML = data.message;
+            }, //function(data, textStatus, jqXHR)
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown); //function(jqXHR, textStatus, errorThrown)
+            }
+        });
+    }
+
+    function showOutput(message, status) {
+        var e = document.getElementById("output");
+        e.style.display = 'block';
+        document.getElementById("message").innerHTML = message;
+        document.getElementById("status").innerHTML = status;
+    }
+
+    $(function () {
+
+        $('a[href^="#"]').click(function (event) {
+            var id = $(this).attr("href");
+            var offset = 20;
+            var target = $(id).offset().top - offset;
+
+            $('html, body').animate({
+                scrollTop: target
+            }, 500);
+            event.preventDefault();
+        });
+
     });
-}
 
- function showOutput(message, status) {
-       var e = document.getElementById("output");
-       e.style.display = 'block';
-       document.getElementById("message").innerHTML = message;
-       document.getElementById("status").innerHTML = status;
- }
-
-$(function () {
-
-$('a[href^="#"]').click(function(event) {
-var id = $(this).attr("href");
-var offset = 20;
-var target = $(id).offset().top - offset;
-
-$('html, body').animate({scrollTop:target}, 500);
-event.preventDefault();
-});
-
-});
-    
 });
