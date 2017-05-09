9d9e2f3f878a7b06f2c4f3b3b826c0e0038d1468
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-05-09 05:01:13 -0700

    Added handlers for get list and get status

--- DeployerCli/deployercli.py
+++ DeployerCli/deployercli.py
@@ -2,6 +2,11 @@
 
 import sys
 import argparse
+import requests
+import json
+
+SUCCESS = 0
+FAILURE = 1
 
 # Helper routine for dumping argument info
 def dbgPrintAllArgs(args):
@@ -13,11 +18,45 @@ def dbgPrintAllArgs(args):
 
 
 def getPiStatusHandler(args):
-    pass
+    url = 'http://' + args.serverIp + '/deployer/v1/' + args.piIp + '/status'
+    response = requests.get(url)
+    response.encoding = 'ISO-8859-1'
+
+    if response.status_code == 404:
+        print 'Invalid Raspberry Pi IP address specified'
+        return FAILURE
+
+    try:
+        res = json.loads(response.content)
+        print "RaspberryPi IP: %s" % args.piIp
+        for package in res['packages']:
+            print '    Package URL: %s' % package['url']
+            print '    Package Status: %s' % package['status']
+            print ''
+
+        return SUCCESS
+
+    except Exception as e:
+        print 'Error processing JSON. %s' % e.message
+        return FAILURE
 
 
 def getPiListHandler(args):
-    pass
+    url = 'http://' + args.serverIp + '/deployer/v1/list'
+    response = requests.get(url)
+    response.encoding = 'ISO-8859-1'
+
+    try:
+        res = json.loads(response.content)
+        print 'Registered Raspberry Pi(s) IP Addresses:'
+        for pi in json.loads(response.content):
+            print '    %s' % pi
+
+        return SUCCESS
+
+    except Exception as e:
+        print 'Error processing JSON. %s' % e.message
+        return FAILURE
 
 
 def deployAppHandler(args):
@@ -32,7 +71,7 @@ handlers = {
 
 
 def action(args):
-    handlers[args.cmd](args)
+    return handlers[args.cmd](args)
 
 
 def argEval(args):
@@ -89,6 +128,6 @@ def getArgs():
 if __name__ =='__main__':
     args = getArgs()
     if args == None:
-        sys.exit(1)
+        sys.exit(FAILURE)
         
-    action(args)
+    sys.exit(action(args))
