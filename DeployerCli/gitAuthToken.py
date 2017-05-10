4810f7022eceb9e5bbc0dd9cb17d14b437c09655
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-05-10 14:03:34 -0700

    Add scope to Github authentication token request and code to test webhook create

--- DeployerCli/gitAuthToken.py
+++ DeployerCli/gitAuthToken.py
@@ -5,6 +5,30 @@ from requests.auth import HTTPBasicAuth
 import string
 import random
 
+'''
+# Function to test webhook create
+def callwebhook(user_name, repo_name, TOKEN):
+    payload = {
+              "name"    : "web",
+              "active"  : True,
+              "events": [ "push" ],
+              "config": {
+                  #"url": "https://104.196.235.71/deployer/v1/webhookresponse",
+                  "url": "https://104.196.235.71/webhook",
+                  "content_type" : "json"
+                  }
+              }
+    print json.dumps(payload)
+
+    headers = {'Authorization' : 'basic ' + TOKEN}
+    url = 'https://api.github.com/repos/' + user_name + '/' + repo_name + '/hooks'
+    print url
+    response_data = requests.post(url, data=json.dumps(payload), headers = headers)
+
+    response_data.encoded='ISO-8859-1'
+    print response_data.text
+'''
+
 ''' Generate a unique note for every authorization token request '''
 def uniqueNoteGenerator(size=4, chars=string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -14,30 +38,42 @@ def uniqueNoteGenerator(size=4, chars=string.ascii_uppercase):
 def getAuthToken():
     username = raw_input('Github username: ')
     password = getpass.getpass('Github password: ')
-    
+
+    # Used with callwebhook
+    # reponame = raw_input('Github repo name: ')
+
     url = 'https://api.github.com/authorizations' 
+
+    # 'note' should be unique for every auth token request
     note = uniqueNoteGenerator()
+
     # 'note' is required in every request.
     #  rest of the parameters are optional
     payload = {
-                "note": note  
+                  "scopes": [
+                      "public_repo"
+                  ],
+                  "note": note
               }
-    
+    # authenticate with username and password
     res = requests.post(url,
                         auth = HTTPBasicAuth(username, password),
-                        data = json.dumps(payload))
-    
+                        data = json.dumps(payload)
+                       )
+
     res.encoded='ISO-8859-1'
     res = json.loads(res.content)
     keys = res.keys()
 
+    # upon unsuccessful request, a message is returned
     if 'message' in keys:
         return None
 
+    # upon successful request, a timestamp is generated in the field 'created_at'
     elif 'created_at' in keys:
         auth_token = res['token']
 
-    # TODO: integrate this token with the rest api call from cli
+    # callwebhook(username, reponame, auth_token)
 
     return auth_token
 
